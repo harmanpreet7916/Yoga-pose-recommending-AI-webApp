01079aa50a8e48ac7b723bba5307ffaddfb23843
HARMANPREET SINGH <77985528+harmanpreet7916@users.noreply.github.com>
2023-11-26 16:25:23 +0530

    deleted redundant code from image server

--- server/api/routes/ImageRoute.js
+++ server/api/routes/ImageRoute.js
@@ -3,10 +3,10 @@ const app = require('../../app').default;
 const router = express.Router();
 
 router.get('/',(req,res,next)=>{
-    res.status(200).json({message:"chal da e oye"})
+    res.status(200).json({message:"get route is working"})
 })
 router.post('/',(req,res,next)=>{
-    res.status(200).json({message:"post v chaldda"})
+    res.status(200).json({message:"post post route is working"})
 
 })
 
