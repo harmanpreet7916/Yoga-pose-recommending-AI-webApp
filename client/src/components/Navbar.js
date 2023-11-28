eb2d0f3ebd81310d28fdaf96c1aa264212a402eb
HARMANPREET SINGH <77985528+harmanpreet7916@users.noreply.github.com>
2023-11-28 23:36:28 +0530

    removed extra images from server folder and changed navbar hrefs to links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from 'react-router-dom'
 export default function Navbar() {
   return (
     <div className="dark:bg-gray-900 fixed w-full">
@@ -8,21 +9,19 @@ export default function Navbar() {
             </div>
             <div className="flex items-center">
                 <nav className="items-center hidden text-lg text-gray-800 uppercase font-sen dark:text-white lg:flex">
-                    <a href="/" className="flex px-6 py-2">
+                    <Link to="/" className="flex px-6 py-2">
                         Home
-                    </a>
-                    <a href="/services" className="flex px-6 py-2">
+                    </Link>
+                    <Link to="/services" className="flex px-6 py-2">
                         Services
-                    </a>
-                    <a href="/" className="flex px-6 py-2">
+                    </Link>
+                    <Link to="/" className="flex px-6 py-2">
                         Product
-                    </a>
-                    <a href="/" className="flex px-6 py-2">
+                    </Link>
+                    <Link to="/" className="flex px-6 py-2">
                         Contact
-                    </a>
-                    <a href="/" className="flex px-6 py-2">
-                        Carrer
-                    </a>
+                    </Link>
+                 
                 </nav>
                 <button className="flex flex-col ml-4 lg:hidden">
                     <span className="w-6 h-1 mb-1 bg-gray-800 dark:bg-white">
